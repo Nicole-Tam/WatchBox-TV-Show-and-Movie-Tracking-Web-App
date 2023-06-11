26e67d4425bfce8c5c41bd8e2730b52b471984bc
Nicole Tam <nicoletamtv@gmail.com>
2023-06-12 02:22:32 +1000

    updating initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+   })
+
+let server = app.listen(8888, function(){
+ console.log("App server is running on port 8888");
+});
+
