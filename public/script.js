3c19effe280c32bfeb9f49f375de19bd7b131063
Nicole Tam <nicoletamtv@gmail.com>
2023-06-17 04:03:44 +1000

    added js for overlay closing when mouse out

--- public/script.js
+++ public/script.js
@@ -1,34 +1,42 @@
 const addButton = document.getElementById("add-content-button");
-const popup = document.getElementById("content-popup-background");
+const overlay = document.getElementById("content-popup-background");
+const popup = document.getElementById('content-popup');
 const closePopup = document.getElementById("form-close");
 
 addButton.addEventListener("click", () => {
-  popup.style.display = "flex";
+  overlay.style.display = "flex";
+  popup.style.display = 'flex';
 });
 
 closePopup.addEventListener("click", () => {
-  popup.style.display = "none"
-})
+  overlay.style.display = "none"
+});
 
 document.addEventListener('keydown', function(e){
-
   console.log(e.key);
   if(e.key =='Escape'){
-  closePopup();
+    overlay.style.display = "none";
   }
-})
+});
+
+overlay.addEventListener('click', function(event) {
+  if (event.target === overlay) {
+    overlay.style.display = 'none';
+    popup.style.display = 'none';
+  }
+});
 
 var movieButton = document.getElementById("movie-button");
 var tvshowButton = document.getElementById("tvshow-button");
 var runtime = document.getElementById("runtime");
 var showLength = document.getElementById("show-length");
 
-movieButton.addEventListener("click", function () {
+movieButton.addEventListener("click", function () { //don't show movie options when tv show button is pressed
   runtime.style.display = "flex";
   showLength.style.display = "none";
 });
 
-tvshowButton.addEventListener("click", function () {
+tvshowButton.addEventListener("click", function () { //don't show tv show options when movie button is pressed
   runtime.style.display = "none";
   showLength.style.display = "flex";
 });
