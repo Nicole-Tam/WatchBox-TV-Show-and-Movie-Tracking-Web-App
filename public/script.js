670bdbe6d78cf4a8754e5e9281d656259b74e004
Nicole Tam <nicoletamtv@gmail.com>
2023-06-19 06:38:03 +1000

    adding javascript local storage

--- public/script.js
+++ public/script.js
@@ -40,3 +40,63 @@ tvshowButton.addEventListener("click", function () { //don't show tv show option
   runtime.style.display = "none";
   showLength.style.display = "flex";
 });
+
+
+//retrieving form data
+
+// const mediaType = form.elements['media-type'].value;
+// const title = form.elements['title'].value;
+// const genre = form.elements['genre'].value;
+// const description = form.elements['description'].value;
+// const review = form.elements['review'].value;
+// const rating = form.elements['rating'].value;
+// const runtime = form.elements['runtime'].value;
+// const seasons = form.elements['seasons'].value;
+// const episodes = form.elements['episodes'].value;
+// const favorites = form.elements['favourites'].checked;
+// const watched = form.elements['watched'].checked;
+// const toWatch = form.elements['to-watch'].checked;
+
+let formData = {
+  mediaType,
+  title,
+  genre,
+  description,
+  review,
+  rating,
+  runtime,
+  seasons,
+  episodes,
+  favorites,
+  watched,
+  toWatch
+};
+
+localStorage.setItem('formData', JSON.stringify(formData));
+
+form.reset();
+});
+
+
+const form = document.querySelector('#content-form');
+
+form.addEventListener("submit", function (event) {
+event.preventDefault();
+
+if (form.elements.mediaType.value === "movie"){
+addMovie(
+    form.elements.mediaType.value,
+    form.elements.title.value,
+    form.elements.genre.value,
+    form.elements.description.value,
+    form.elements.review.value,
+    form.elements.rating.value,
+    form.elements.runtime.value,
+    form.elements.favorites.value,
+    form.elements.watched.value,
+    form.elements.toWatch.value
+)
+
+}
+})
+
