cf826c7e5c4ff48fffb8c50db24c818ee48eb8b0
Nicole Tam <nicoletamtv@gmail.com>
2023-06-19 17:21:20 +1000

    fixed syntax error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/dist/index.html'))
+    res.sendFile(__dirname + '/dist/index.html')
    })
 
 let server = app.listen(8888, function(){
