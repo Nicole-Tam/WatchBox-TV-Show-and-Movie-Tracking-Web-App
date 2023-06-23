8cfc97c94e72125c739a37e4f25836712bbb24fe
Nicole Tam <nicoletamtv@gmail.com>
2023-06-23 13:26:27 +1000

    fixed app.use file path

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
-   })
+app.use('/assets', express.static(__dirname + '/public/assets'));
 
-let server = app.listen(8888, function(){
- console.log("App server is running on port 8888");
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
-
-
-    
+let server = app.listen(8888, function() {
+  console.log("App server is running on port 8888");
+});
